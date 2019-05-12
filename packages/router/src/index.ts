18d27636cfdb572f67bc7c477056de817154cb70
Genadi Sokolov <genadi@annoto.net>
2019-05-12 11:48:28 +0300

    export more methods and interfaces

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -6,7 +6,15 @@ export {
   RouterHistory,
   MatchOptions,
   MatchResults,
-  RouteRenderProps
+  RouteRenderProps,
+  RouteViewOptions,
+  HistoryType,
+  Prompt,
 } from './global/interfaces';
+export { default as ActiveRouterProviderConsumer, ActiveRouterState } from './global/active-router';
 export { Components } from './components';
 export { matchPath } from './utils/match-path';
+export { createLocation, createKey } from './utils/location-utils';
+export { parseQueryString, createPath } from './utils/path-utils';
+export { default as createBrowserHistory, CreateBrowserHistoryOptions } from './utils/createBrowserHistory';
+export { default as createHashHistory, CreateHashHistoryOptions } from './utils/createHashHistory';
